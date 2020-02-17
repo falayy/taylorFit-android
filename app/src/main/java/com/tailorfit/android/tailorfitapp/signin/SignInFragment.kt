157635e08de84440674e8a50c3e559de806f98ad
Falaye Iyanuoluwa <falaycornelius@gmail.com>
2020-02-17 12:18:28 +0100

    implements sign up and sign in page bar edit text final touch

---
+++ app/src/main/java/com/tailorfit/android/tailorfitapp/signin/SignInFragment.kt
@@ -0,0 +1,22 @@
+package com.tailorfit.android.tailorfitapp.signin
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tailorfit.android.R
+/**
+ * A simple [Fragment] subclass.
+ */
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+
+}
